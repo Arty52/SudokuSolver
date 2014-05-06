4d7dfed9b4c9309cf1873c49bab06ea9d5c1947a
Arty52 <artgrichine@gmail.com>
2014-05-05 17:52:10 -0700

    solver implementation beginning

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -3,29 +3,38 @@
 
 import sys
 
+class solver:
+    def __init__(self, board):
+        self._board = board
+        l = list(range(1,10))
+        self.u = set(l)
+    
+    def intersection(self):
+        return self.u - self._board.myrow - self._board.mycol - self._board.mysubboard
+        
+
 class Board:
     def __init__(self, puzzle):
-        cells = []
+        self.cells = []
         for c in puzzle:
-            cells.append(Cell(c))
+            self.cells.append(Cell(c))
         
         rows = []
         for r in range(0,81,9):
-            rows.append(MyRow(cells[r:r+9]))
-        
+            rows.append(MyRow(self.cells[r:r+9]))
+            
         cols = []
-        for col in range(9)
+        for col in range(9):
             this_col = []
-            for c in range(col,81,9)
-                this_col.append(cells[c])
-            cols.append(this_col)
+            for c in range(col,81,9):
+                this_col.append(self.cells[c])
+            cols.append(MyCol(this_col))
             
         subboard = []
         for x in range(0,81,27):
             for i in range(0,9,3):
-                for r in range(0,27,9)
-                    subboard.append(MySubBoard(cells[r+i+x:r+i+x+3]))
-  
+                for r in range(0,27,9):
+                    subboard.append(MySubBoard(self.cells[r+i+x:r+i+x+3]))        
 class Cell:
     def __init__(self, item):
         self._item = item
@@ -47,7 +56,6 @@ class Cell:
     def getMySubBoard(self):
         return self.mysubboard
     mysubboard = property(getMySubBoard, setMySubBoard)
-    
 
 class MyRow:
     def __init__(self, cells):
@@ -60,6 +68,7 @@ class MyCol:
         myCells = set(cells)
         for cell in myCells:
             cell.mycol = self
+        print(myCells)
 
 class MySubBoard:
     def __init__(self, cells):
@@ -71,10 +80,14 @@ def main():
     with open(sys.argv[1]) as fh:     #implicitly open and close the file
         for i in fh:
             puzzle = i.rstrip()
-            print(i)
         
-        
-        
+        print()
+        print(puzzle)
+        print()
+        newBoard = Board(puzzle)
+        # print()
+        # print(solver.intersection(newBoard))
+        #         
     
 
 
